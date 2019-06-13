d00661ad083d4b6b5f7cb55f0cbd0c0b0c92d09c
Josh Siva <jtsiva@gmail.com>
2019-06-13 10:27:56 -0400

    removed old project structures

--- BenchmarkCommon/app/src/main/java/edu/nd/cse/benchmarkcommon/UiUpdate.java
+++
@@ -1,7 +0,0 @@
-package edu.nd.cse.benchmarkcommon;
-
-public interface UiUpdate {
-    public void updateText(String text);
-}
-
-
--- Gatt_Client/app/src/main/java/edu/nd/cse/gatt_client/BenchmarkProfileClient.java
+++
@@ -1,8 +0,0 @@
-package edu.nd.cse.gatt_client;
-
-/**
- * This class implements the behavior of the client-side interactions
- * with the benchmark profile
- */
-public class BenchmarkProfileClient {
-}
