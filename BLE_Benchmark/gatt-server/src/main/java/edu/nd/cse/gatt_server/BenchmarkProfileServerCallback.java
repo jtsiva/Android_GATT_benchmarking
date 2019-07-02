d767d569a6f3ee5ae6de82006cca89b90c6a60b9
Josh Siva <jtsiva@gmail.com>
2019-07-02 12:41:25 -0400

    added callback iface for server profile

---
+++ BLE_Benchmark/gatt-server/src/main/java/edu/nd/cse/gatt_server/BenchmarkProfileServerCallback.java
@@ -0,0 +1,8 @@
+package edu.nd.cse.gatt_server;
+
+/**
+ * The intention of this callback interface is to allow the server side
+ * of the profile to provide updates to the application layer.
+ */
+public interface BenchmarkProfileServerCallback {
+}
