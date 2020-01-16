2fa637d8b25a0743037e93ba3d66e03856ec070b
Josh Siva <jtsiva@gmail.com>
2020-01-16 17:29:39 -0500

    Updated UI and manifest
    Simplified UI to a single scrolling text view
    Corrected issues with the manifest so that the app actually
    loads. Included the permissions code from the previous app.

--- BLE_Benchmark/gatt-client/src/main/java/edu/nd/cse/gatt_client/BenchmarkClient.java
+++ BLE_Benchmark/gatt-client/src/main/java/edu/nd/cse/gatt_client/BenchmarkClient.java
@@ -1,5 +1,9 @@
 package edu.nd.cse.gatt_client;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.widget.TextView;
 import android.app.Activity;
 import android.util.Log;
@@ -15,25 +19,75 @@ import java.util.Date;
  */
 public class BenchmarkClient extends Activity{
     /* UI elements */
-    private TextView mParameters;
+
     private TextView mUpdates;
 
     /* Profile related things */
     private BenchmarkProfileClient mBenchmarkClient;
 
 
+    /**
+     * Convenience method to write text to the screen
+     * @param text - the text to write to the screen
+     */
+    private void writeUpdate(final CharSequence text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mUpdates.append(text);
+                mUpdates.append("\n");
+            }
+        });
+    }
+
+    /**
+     * Convenience method to determine if the user has granted this application
+     * the necessary permissions to be able to run.
+     *
+     * @param context app context
+     * @param permissions the permissions to check
+     * @return true if we have the permissions, false otherwise
+     */
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Method that checks for permissions and if app has not been given one of
+     * the permissions, ask the user to grant the permission.
+     */
+    private void checkPermissions(){
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        checkPermissions();
 
-        mParameters = (TextView) findViewById(R.id.params);
         //Bundle receiveBundle = this.getIntent().getExtras();
-        mParameters.append("Parameters:\n");
-        //Here we would append a text version of all of the parameters
+
 
 
         mUpdates = (TextView) findViewById(R.id.updates);
+
+        writeUpdate("Parameters:\n");
+        //Here we would append a text version of all of the parameters
 
         // Devices with a display should not go to sleep
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -41,26 +95,14 @@ public class BenchmarkClient extends Activity{
         mBenchmarkClient = new BenchmarkProfileClient (this, new BenchmarkProfileClientCallback () {
             @Override
             public void onBenchmarkStart () {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Timestamp ts = new Timestamp(new Date().getTime());
-                        mUpdates.append("Benchmark started at: " + ts);
-                        mUpdates.append("\n");
-                    }
-                });
+                Timestamp ts = new Timestamp(new Date().getTime());
+                writeUpdate("Benchmark started at: " + ts);
             }
 
             @Override
             public void onBenchmarkComplete (){
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Timestamp ts = new Timestamp(new Date().getTime());
-                        mUpdates.append("Benchmark completed at: " + ts);
-                        mUpdates.append("\n");
-                    }
-                });
+                Timestamp ts = new Timestamp(new Date().getTime());
+                writeUpdate("Benchmark completed at: " + ts);
 
                 mBenchmarkClient.requestThroughput();
             }
@@ -72,40 +114,29 @@ public class BenchmarkClient extends Activity{
 
             @Override
             public void onThroughputAvailable (final float throughput){
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mUpdates.append("Throughput: " + throughput);
-                        mUpdates.append("\n");
-                    }
-                });
+                writeUpdate("Throughput: " + throughput);
             }
 
             @Override
             public void onLossRateAvailable (final float lossRate){
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mUpdates.append("Loss Rate: " + lossRate);
-                        mUpdates.append("\n");
-                    }
-                });
+                writeUpdate("Loss Rate: " + lossRate);
             }
 
             @Override
             public void onBenchmarkError (final int code, final String details) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mUpdates.append("Error " + code + ": " + details);
-                        mUpdates.append("\n");
-                    }
-                });
+                writeUpdate("Error " + code + ": " + details);
             }
         });
 
+        Log.v("gatt", "about to start!");
+
         mBenchmarkClient.prepare(); //set up testing params with default values
         mBenchmarkClient.beginBenchmark(); //run default length of 10 seconds
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
     }
 
     @Override
